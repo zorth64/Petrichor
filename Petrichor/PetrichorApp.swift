57936a747fb4df62f50928d1c5284846a9b7552b
Kushal Pandya <kushalspandya@gmail.com>
2025-04-19 18:28:49 -0400

    Initial Commit

---
+++ Petrichor/PetrichorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetrichorApp.swift
+//  Petrichor
+//
+//  Created by Kushal Pandya on 2025-04-19.
+//
+
+import SwiftUI
+
+@main
+struct PetrichorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
